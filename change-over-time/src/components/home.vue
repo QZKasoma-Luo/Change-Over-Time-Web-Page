62a77009954368e5a14331e3f5c531a4fe7d2929
QZL <98981724+QZKasoma-Luo@users.noreply.github.com>
2023-02-12 06:01:39 -0800

    re-structure the VUE, update from VUE 2..0 to VUE 3.0 and import element-plus

---
+++ change-over-time/src/components/home.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>{{ home_page_message }}</p>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            home_page_message: 'Welcome to change-over-time dashboard!'
+
+        }
+    },
+}
+</script>
